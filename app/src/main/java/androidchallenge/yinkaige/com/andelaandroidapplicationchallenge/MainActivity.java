b450a5a5eab50c411d7b379a0ec829bb77a8fb35
Ige Oluyinka <scolaguys@yahoo.co.uk>
2017-10-26 16:28:59 +0100

    Added Menu Items

---
+++ app/src/main/java/androidchallenge/yinkaige/com/andelaandroidapplicationchallenge/MainActivity.java
@@ -0,0 +1,37 @@
+package androidchallenge.yinkaige.com.andelaandroidapplicationchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
